28b97a4721899d9d9483c1c2a471180bfbadda6d
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-02-13 20:13:03 +0100

    Add getting data from database

---
+++ databaseExampleQuery.sql
@@ -0,0 +1,23 @@
+('Model', '29', 'Prototyping', 'Modeling', 'Improvements in image capturing, modeling and
+    computer vision will make VR and AR more efficient. These trends will help make 3D a critical part
+    of future eCommerce.', 'Developing 3D marketing materials: VR and AR can be used in your
+    marketing to tell your brands story, showcase your products and
+    create promotional content. ');
+
+('Render', '69', 'product images', 'Rendering', 'Improvements in image capturing, modeling and
+    computer vision will make VR and AR more efficient. These trends will help make 3D a critical part
+    of future eCommerce.', 'Developing 3D marketing materials: VR and AR can be used in your
+    marketing to tell your brands story, showcase your products and
+    create promotional content. ');
+
+('Animate', '169', 'digital content', 'Animation', 'Improvements in image capturing, modeling and
+    computer vision will make VR and AR more efficient. These trends will help make 3D a critical part
+    of future eCommerce.', 'Developing 3D marketing materials: VR and AR can be used in your
+    marketing to tell your brands story, showcase your products and
+    create promotional content. ');
+
+('Visualize', '229', 'brand recognition', 'Product Visualisation', 'Improvements in image capturing, modeling and
+    computer vision will make VR and AR more efficient. These trends will help make 3D a critical part
+    of future eCommerce.', 'Developing 3D marketing materials: VR and AR can be used in your
+    marketing to tell your brands story, showcase your products and
+    create promotional content. ');
